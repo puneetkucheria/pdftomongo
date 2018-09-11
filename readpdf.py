8b710cddf3be233429480da0a4270dff1c7c1da4
Puneet Kucheria <puneetkucheria@gmail.com>
2018-09-11 15:06:44 +0000

    cor

--- readpdf.py
+++ readpdf.py
@@ -1,13 +1,13 @@
 from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import PDFPageAggregator
+from pdfminer.converter import PDFPageAggregator 
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage
-#from pdfminer.ImageWriter import image_export
 from pymongo import MongoClient
 import datetime
 import os
+from io import StringIO
 from binascii import b2a_hex
-#from pil import image
+from PIL import Image
 
 connection = MongoClient()
 
@@ -24,7 +24,6 @@ def write_file (folder, filename, filedata, flags='w'):
         except IOError:
             pass
         return result
-
 
 def determine_image_type (stream_first_4_bytes):
     #Find out the image file type based on the magic number comparison of the first 4 (or 2) bytes
@@ -43,20 +42,36 @@ def determine_image_type (stream_first_4_bytes):
         file_type = '.bmp'
         print("found bmp")
     else:
-#        file_type = '.jpg'
+        file_type = '.jpeg'
         print("filetype not found")
     return file_type
 
+
+
+
+
 def save_image (lt_image, page_number, images_folder):
-    print(lt_image)
     #Try to save the image data from this LTImage object, and return the file name, if successful
     result = None
     if lt_image.stream:
-        file_stream = lt_image.stream.get_rawdata()
+        file_stream = lt_image.stream.get_data()
+#        file_stream = lt_image.bits
+#        print(str(lt_image.x0)+' '+str(lt_image.y0))
+#        print(str(lt_image.x1)+' '+str(lt_image.y1))
+#        print(lt_image.)
+        (x0,y0,x1,y1) = lt_image.bbox
+        print(x0)
+        print(y0)
+        print(x1)
+        print(y1)
+
+        (width, height) = lt_image.srcsize
+        print('height: '+str(height)+'      width :'+str(width)+'    image name:'+str(lt_image.name))
         file_ext = determine_image_type(file_stream[0:4])
+        print(file_ext)
         if file_ext:
             file_name = ''.join([str(page_number), '_', lt_image.name, file_ext])
-            if write_file(images_folder, file_name, lt_image.stream, flags='wb'):#.stream.get_rawdata(), flags='wb'):
+            if write_file(images_folder, file_name, file_stream, flags='wb'):
                 result = file_name
                 print("image saved"+result)
     return result
@@ -64,20 +79,22 @@ def save_image (lt_image, page_number, images_folder):
 
 
 def save_file_text(path, page, layout, text, date):
-#    connection = MongoClent()
     db = connection.db_pdftomongo.col_text_dump
     post = {'file':path, 'pagenumber':page, 'layout':layout, 'text':text, 'createdon':date}
-    post_id = db.insert_one(post).inserted_id
-    print('file saved with id = '+ str(post_id))
+    print(post)
+#    post_id = db.insert_one(post).inserted_id
 
-#def save_image(lt_obj):
-#    print("image found")
+
 
 def find_images_in_obj(outer_layout, pagenumber, images_folder):
+    print('infigure')
     for thing in outer_layout:
+        print('thing found')
+        print(thing)
         if isinstance(thing, LTImage):
-#            print("found image in figure")
             save_image(thing, pagenumber, images_folder)
+
+
 
 
 path = 'RetailPharmacyInventory.pdf'
@@ -89,13 +106,13 @@ doc.set_parser(parser)
 doc.initialize('')
 rsrcmgr = PDFResourceManager()
 laparams = LAParams()
-#laparams.char_margin = 1.0
-#laparams.word_margin = 1.0
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+images_folder = 'images'
+imagewriter = None
+outfp = StringIO()
+device = PDFPageAggregator(rsrcmgr,laparams=laparams)
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 extracted_text = ''
 
-images_folder = "images"
 
 # Process each page contained in the document.
 for pagenumber, page in enumerate(doc.get_pages()):
@@ -104,19 +121,18 @@ for pagenumber, page in enumerate(doc.get_pages()):
     interpreter.process_page(page)
     layout = device.get_result()
     for lt_obj in layout:
-        if isinstance(lt_obj, LTTextBox):
-            extracted_text = lt_obj.get_text()
-            save_file_text(path,pagenumber,"LTTextBox",extracted_text,datetime.datetime.now())
-        if isinstance(lt_obj, LTTextLine):
-            extracted_text = lt_obj.get_text()
-            if not extracted_text==" \n" or not extracted_text=="\n":
-                save_file_text(path,pagenumber,"LTTextline",extracted_text,datetime.datetime.now()) 
+#        print(lt_obj.x0)
+#        if isinstance(lt_obj, LTTextBox):
+#            extracted_text = lt_obj.get_text()
+#            save_file_text(path,pagenumber,"LTTextBox",extracted_text,datetime.datetime.now())
+#        if isinstance(lt_obj, LTTextLine):
+#            extracted_text = lt_obj.get_text()
+#            if not extracted_text==' \n' and not extracted_text=='\n':
+#                save_file_text(path,pagenumber,"LTTextline",extracted_text,datetime.datetime.now()) 
         if isinstance(lt_obj, LTFigure):
-#            print("found figure")
+            print('found figure')
             find_images_in_obj(lt_obj, pagenumber, images_folder)
         if isinstance(lt_obj, LTImage):
-#            print("found image")
+#            print(b2hex(lt_obj))
+            print('x0 ='+str(lt_obj.x0))
             save_image(lt_obj,pagenumber, images_folder)
-#               print(extracted_text)
-#with open('convertedFile.txt',"wb") as txt_file:
-#    txt_file.write(extracted_text.encode("utf-8"))
